329a9f1a654309ed434f25255d25630b20b02efb
mtk3281 <tkmunthasar@gmail.com>
2024-08-26 22:55:07 +0530

    many changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Notepad',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0
+    }
+  }
 };
 
 export default config;
